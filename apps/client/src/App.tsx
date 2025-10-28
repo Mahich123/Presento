980e30f1ff2c8df5ec7c84dc36f6f2ccf43863d5
mahi <mahichowdhury587@gmail.com>
2025-10-28 21:28:21 +0600

    chore: project scaffold with hono, daisyui

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>Presento</h1>
+    </div>
+  )
+}
+
+export default App
